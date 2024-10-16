cd3c5587f72aa80a578d00bf04e88fce8b79518c
Gabriel-Manzano <gabriel.mzn.r.xx66@gmail.com>
2024-10-16 01:45:46 -0600

    Repaso Laravel - 3
    3.- Crea una vista de inicio al estilo de una portada con tus datos.
    
    (Se volvió a crear el proyecto por problemas con el servidor)

---
+++ practicaRepasos/resources/views/welcome.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    @vite(['resources/js/app.js'])
+
+    <title>Document</title>
+</head>
+<body>
+    <center>
+    <br>
+    <h1>Universidad Politécnica de Querétaro</h1>
+    <br>
+    <h2>Ingeniería en Sistemas Computacionales</h2>
+    <br>
+    <br>
+    <h2>Materia</h2>
+    <br>
+    <h3>Programación Web</h3>
+    <br>
+    <h2>Tema</h2>
+    <br>
+    <h3>Repaso 1 Laravel</h3>
+    <br>
+    <h2>Alumno</h2>
+    <br>
+    <h3>Manzano Ruíz Gabriel</h3>
+    <br>
+    <a href="{{ route('rutarepaso1') }}" class="btn btn-primary">Repaso 1</a>
+    <br>
+    <br>
+    <h4>16/10/2024</h4>
+    </center>
+</body>
+</html>
