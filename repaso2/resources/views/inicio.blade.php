669853c235cad71da34f0c8296b81cd3ecbb67ab
Gabriel-Manzano <gabriel.mzn.r.xx66@gmail.com>
2024-10-29 18:44:06 -0600

    Repaso 2 - Paso 3 (Navbar/Plantilla)
    Creación de una navbar y de una plantillas

--- repaso2/resources/views/inicio.blade.php
+++ repaso2/resources/views/inicio.blade.php
@@ -1,14 +1,25 @@
+@extends('layouts.plantilla1')
+@section('titulo')
+@section('contenido')
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     @vite(['resources/js/app.js'])
 
-    <title>Document</title>
+    <title>Inicio</title>
 </head>
+
 <body>
-    
+
+    <center>
+        <h1>Hola mundo</h1>
+        <button type="button" class="btn btn-outline-info">Info</button>
+    </center>
 </body>
+
 </html>
+@endsection
---
+++ repaso2/resources/views/layouts/plantilla1.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('titulo')</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+        <ul class="nav nav-pills">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Active</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+        </ul>
+    </nav>
+    @yield('contenido')
+</body>
+
+</html>
